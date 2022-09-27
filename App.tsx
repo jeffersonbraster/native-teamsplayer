d495416fd5cd71ee0fc00247ea3f32ac2487d76c
Jefferson <jeferson.oliveira@g.globo>
2022-09-26 22:47:21 -0300

    carregando fonts personalizadas com expo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from "react-native";
 import { Groups } from "@screens/Groups";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
